4a8522733b35194f5369cdf10404aece2385800b
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-03-14 02:39:04 +0100

    Create OpenAiConfiguration

---
+++ src/OpenAi.Client/OpenAiConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using OpenAI_API.Models;
+
+namespace OpenAi.Client;
+
+public sealed class OpenAiConfiguration
+{
+    public const string SectionName = "OpenAiConfiguration";
+    
+    [Required]
+    public string ApiKey { get; set; } = null!;
+    
+    public Model AiModelType { get; set; } = Model.ChatGPTTurbo;
+}
