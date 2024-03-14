07892c77e3d083472d0a422bd7a711bc861d1c22
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-03-14 02:40:16 +0100

    Register OpenAiclient

---
+++ src/OpenAi.Client/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using OpenAI_API;
+using OpenAi.Client.Services;
+
+namespace OpenAi.Client.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddAiClient(this IServiceCollection services)
+    {
+        services.AddOptions<OpenAiConfiguration>()
+            .BindConfiguration(OpenAiConfiguration.SectionName)
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+        services.AddOpenAiClient();
+
+        return services;
+    }
+
+    private static IServiceCollection AddOpenAiClient(this IServiceCollection services)
+    {
+        services.AddTransient<IOpenAIAPI, OpenAIAPI>(p =>
+        {
+            var config = p.GetRequiredService<IOptions<OpenAiConfiguration>>().Value;
+            return new OpenAIAPI(config.ApiKey);
+        });
+
+        return services;
+    }
+}
